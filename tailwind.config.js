7bd8df582be779a2b22c2a426c1eb0bc6484b74d
Warren Dodsworth <warren.dodsworth@gmail.com>
2026-04-12 08:25:00 +1000

    chore: config update - tsconfig, package.json etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,ts,js}'],
-  darkMode: 'media', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+module.exports = {};
